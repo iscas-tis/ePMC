4eb8706c540391a6e5086f36399d6c2b8ca962d7
Andrea Turrini <andrea.turrini@gmail.com>
2017-03-10 16:16:05 +0800

    Still on reward renaming for internal use

--- plugins/prism-format/src/main/java/epmc/prism/model/PropertiesImpl.java
+++ plugins/prism-format/src/main/java/epmc/prism/model/PropertiesImpl.java
@@ -99,7 +99,8 @@ public final class PropertiesImpl implements Properties {
                 continue;
             }
             Expression parsed = UtilModelChecker.parseExpression(getContextValue(), definition);
-            parsed = PRISM2JANIConverter.useNamedRewardsOnly(model, parsed);
+//            This should be disabled, otherwise there are inconsistencies with the prefix appended in front to the reward name
+//            parsed = PRISM2JANIConverter.useNamedRewardsOnly(model, parsed);
             properties.put(prop, parsed);
         }
         for (Entry<String,String> entry : rawProperties.getConstants().entrySet()) {
