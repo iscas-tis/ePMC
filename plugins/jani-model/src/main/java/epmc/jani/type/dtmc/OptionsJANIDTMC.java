86df9e6eff695547e50807a8246db1639c238d43
emhahn <emhahn@10.0.1.104>
2017-08-16 16:22:44 +0800

    correction of successor probability computation for dtmcs

--- plugins/jani-model/src/main/java/epmc/jani/type/dtmc/OptionsJANIDTMC.java
+++ plugins/jani-model/src/main/java/epmc/jani/type/dtmc/OptionsJANIDTMC.java
@@ -23,6 +23,4 @@ package epmc.jani.type.dtmc;
 public enum OptionsJANIDTMC {
 	/** Base name of resource file for options description. */
 	OPTIONS_JANI_DTMC,
-	/** Option whether to allow multiple transitions remaining for DTMCs, */
-    JANI_DTMC_ALLOW_MULTI_TRANSITION,
 }
