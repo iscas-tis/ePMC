77354c3873067c97bc4a07f89dcfde331c76f099
emhahn <emhahn@10.0.1.104>
2017-08-17 11:35:14 +0800

    cleanup

--- plugins/graphsolver-iterative/src/main/java/epmc/graphsolver/iterative/GraphSolverIterativeNative.java
+++ plugins/graphsolver-iterative/src/main/java/epmc/graphsolver/iterative/GraphSolverIterativeNative.java
@@ -44,7 +44,6 @@ import epmc.graphsolver.objective.GraphSolverObjectiveExplicitBounded;
 import epmc.graphsolver.objective.GraphSolverObjectiveExplicitBoundedCumulative;
 import epmc.graphsolver.objective.GraphSolverObjectiveExplicitBoundedCumulativeDiscounted;
 import epmc.graphsolver.objective.GraphSolverObjectiveExplicitBoundedReachability;
-import epmc.graphsolver.objective.GraphSolverObjectiveExplicitUnbounded;
 import epmc.graphsolver.objective.GraphSolverObjectiveExplicitUnboundedCumulative;
 import epmc.graphsolver.objective.GraphSolverObjectiveExplicitUnboundedReachability;
 import epmc.messages.OptionsMessages;
@@ -193,7 +192,6 @@ public final class GraphSolverIterativeNative implements GraphSolverExplicit {
     			&& !(objective instanceof GraphSolverObjectiveExplicitBoundedCumulative)
     			&& !(objective instanceof GraphSolverObjectiveExplicitBoundedCumulativeDiscounted)
     			&& !(objective instanceof GraphSolverObjectiveExplicitBoundedReachability)
-    			&& !(objective instanceof GraphSolverObjectiveExplicitUnbounded)
     			&& !(objective instanceof GraphSolverObjectiveExplicitUnboundedCumulative)
     			&& !(objective instanceof GraphSolverObjectiveExplicitUnboundedReachability)
     			) {
@@ -224,8 +222,6 @@ public final class GraphSolverIterativeNative implements GraphSolverExplicit {
         	boundedCumulativeDiscounted();
         } else if (objective instanceof GraphSolverObjectiveExplicitUnboundedReachability) {
             unboundedReachability();
-        } else if (objective instanceof GraphSolverObjectiveExplicitUnbounded) {
-            unbounded();
         } else if (objective instanceof GraphSolverObjectiveExplicitUnboundedCumulative) {
             unboundedCumulative();
         } else {
@@ -382,32 +378,6 @@ public final class GraphSolverIterativeNative implements GraphSolverExplicit {
         IterationStopCriterion stopCriterion = options.getEnum(OptionsGraphSolverIterative.GRAPHSOLVER_ITERATIVE_STOP_CRITERION);
         numIterations = 0;
         GraphSolverObjectiveExplicitUnboundedReachability graphSolverObjectiveUnbounded = (GraphSolverObjectiveExplicitUnboundedReachability) objective;
-        boolean min = graphSolverObjectiveUnbounded.isMin();
-        double precision = options.getDouble(OptionsGraphSolverIterative.GRAPHSOLVER_ITERATIVE_TOLERANCE);
-        if (isSparseMarkovNative(iterGraph) && iterMethod == IterationMethod.JACOBI) {
-            dtmcUnboundedJacobiNative(asSparseMarkov(iterGraph), inputValues, stopCriterion, precision);
-        } else if (isSparseMarkovNative(iterGraph) && iterMethod == IterationMethod.GAUSS_SEIDEL) {
-            dtmcUnboundedGaussseidelNative(asSparseMarkov(iterGraph), inputValues, stopCriterion, precision);
-        } else if (isSparseMDPNative(iterGraph) && iterMethod == IterationMethod.JACOBI) {
-            mdpUnboundedJacobiNative(asSparseNondet(iterGraph), min, inputValues, stopCriterion, precision);
-        } else if (isSparseMDPNative(iterGraph) && iterMethod == IterationMethod.GAUSS_SEIDEL) {
-            mdpUnboundedGaussseidelNative(asSparseNondet(iterGraph), min, inputValues, stopCriterion, precision);  
-        } else {
-            assert false : iterGraph.getClass();
-        }
-        log.send(MessagesGraphSolverIterative.ITERATING_DONE, numIterations,
-                timer.getTimeSeconds());
-    }
-
-    private void unbounded() throws EPMCException {
-        Options options = Options.get();
-        Log log = options.get(OptionsMessages.LOG);
-        StopWatch timer = new StopWatch(true);
-        log.send(MessagesGraphSolverIterative.ITERATING);
-        IterationMethod iterMethod = options.getEnum(OptionsGraphSolverIterative.GRAPHSOLVER_ITERATIVE_METHOD);
-        IterationStopCriterion stopCriterion = options.getEnum(OptionsGraphSolverIterative.GRAPHSOLVER_ITERATIVE_STOP_CRITERION);
-        numIterations = 0;
-        GraphSolverObjectiveExplicitUnbounded graphSolverObjectiveUnbounded = (GraphSolverObjectiveExplicitUnbounded) objective;
         boolean min = graphSolverObjectiveUnbounded.isMin();
         double precision = options.getDouble(OptionsGraphSolverIterative.GRAPHSOLVER_ITERATIVE_TOLERANCE);
         if (isSparseMarkovNative(iterGraph) && iterMethod == IterationMethod.JACOBI) {
