59093242cdfa7582a2332f3497890a12152a21a5
Andrea Turrini <andrea.turrini@gmail.com>
2017-08-22 10:04:58 +0800

    Added memory size support to test script

--- test.sh
+++ test.sh
@@ -2,17 +2,18 @@
 # created on Feb 23rd, 2017
 ROOT="$( cd "$( dirname "$0"  )" && pwd  )"
 TESTDIR="tests/src/test/resources/epmc/"
+: ${EPMC_MEMSIZE:="8G"}
 
 if [ "$1" != "" ]; then
-    java -jar -ea epmc-standard.jar explore --model-input-files $ROOT/$TESTDIR/$1.prism
-    java -jar -ea epmc-standard.jar check --model-input-files $ROOT/$TESTDIR/$1.prism --property-input-files $ROOT/$TESTDIR/$1.pctl
+    java -ea -Xms${EPMC_MEMSIZE} -Xmx${EPMC_MEMSIZE} -jar epmc-standard.jar explore --model-input-files $ROOT/$TESTDIR/$1.prism
+    java -ea -Xms${EPMC_MEMSIZE} -Xmx${EPMC_MEMSIZE} -jar epmc-standard.jar check --model-input-files $ROOT/$TESTDIR/$1.prism --property-input-files $ROOT/$TESTDIR/$1.pctl
 else
     
     echo 'exploring model:'
     for name in `ls $ROOT/$TESTDIR | grep .prism`
     do
         echo "- $name: "
-        java -jar epmc-standard.jar explore --model-input-files $ROOT/$TESTDIR/$name
+        java -Xms${EPMC_MEMSIZE} -Xmx${EPMC_MEMSIZE} -jar epmc-standard.jar explore --model-input-files $ROOT/$TESTDIR/$name
     done
 
 fi
