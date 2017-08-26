f8eaaf6dbeb13ac1f0669b94ccab6feb693e431f
emhahn <emhahn@192.168.43.8>
2017-08-26 23:28:06 +0800

    added new unit test from andrea

--- plugins/propertysolver-multiobjective/src/main/test/java/epmc/multiobjective/ModelNames.java
+++ plugins/propertysolver-multiobjective/src/main/test/java/epmc/multiobjective/ModelNames.java
@@ -33,7 +33,8 @@ public final class ModelNames {
     public final static String MULTI_OBJECTIVE_SIMPLE_REWARDS = PREFIX + "multiObjectiveSimpleRewards.prism";
     public final static String DINNER_REDUCED_PROB_BOUNDED_1 = PREFIX + "dinnerReducedProb-bounded-1.prism";
     public final static String DINNER_REDUCED_BOUNDED_1 = PREFIX + "dinnerReduced-bounded-1.prism";
-
+    public final static String ANDREA_BUG = PREFIX + "andreabug.prism";
+    
     /**
      * Private constructor to prevent instantiation of this class.
      */
