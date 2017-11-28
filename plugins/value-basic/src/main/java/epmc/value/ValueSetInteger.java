eb26486d017934426ea48ed3a1edd90e98ac6498
Ernst Moritz Hahn <ernst.moritz.hahn@gmx.de>
2017-11-28 14:25:54 +0000

    fix

--- plugins/value-basic/src/main/java/epmc/value/ValueSetInteger.java
+++ plugins/value-basic/src/main/java/epmc/value/ValueSetInteger.java
@@ -1,5 +1,17 @@
 package epmc.value;
 
 public interface ValueSetInteger {
+    static boolean is(Value value) {
+        return value instanceof ValueSetInteger;
+    }
+    
+    static ValueSetInteger as(Value value) {
+        if (is(value)) {
+            return (ValueSetInteger) value;
+        } else {
+            return null;
+        }
+    }
+
     void set(int value);
 }
