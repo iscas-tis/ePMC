3665dd5858500d92d54554bfd883608d3ab4b07f
Andrea Turrini <andrea.turrini@gmail.com>
2017-12-09 10:25:01 +0800

    Made identifier final

--- plugins/propertysolver-multiobjective/src/main/java/epmc/multiobjective/graphsolver/GraphSolverIterativeMultiObjectiveWeightedJavaDouble.java
+++ plugins/propertysolver-multiobjective/src/main/java/epmc/multiobjective/graphsolver/GraphSolverIterativeMultiObjectiveWeightedJavaDouble.java
@@ -52,7 +52,7 @@ public final class GraphSolverIterativeMultiObjectiveWeightedJavaDouble implemen
         double diff(double value1, double value2);
     }
 
-    public static String IDENTIFIER = "graph-solver-iterative-multiobjective-weighted-java-double";
+    public static final String IDENTIFIER = "graph-solver-iterative-multiobjective-weighted-java-double";
 
     private GraphExplicit origGraph;
     private GraphExplicit iterGraph;
