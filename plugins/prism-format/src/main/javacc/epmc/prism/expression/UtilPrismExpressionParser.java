ff70c5324660f2a2d39c4902185347867b327777
Ernst Moritz Hahn <ernst.moritz.hahn@gmx.de>
2017-12-10 16:05:06 +0000

    cleanup, more position information

--- plugins/prism-format/src/main/javacc/epmc/prism/expression/UtilPrismExpressionParser.java
+++ plugins/prism-format/src/main/javacc/epmc/prism/expression/UtilPrismExpressionParser.java
@@ -1,7 +1,12 @@
 package epmc.prism.expression;
 
+import java.util.List;
+
 import epmc.error.Positional;
 import epmc.expression.Expression;
+import epmc.expression.standard.ExpressionCoalition;
+import epmc.expression.standard.ExpressionIdentifier;
+import epmc.expression.standard.ExpressionIdentifierStandard;
 import epmc.expression.standard.ExpressionLiteral;
 import epmc.expression.standard.ExpressionOperator;
 import epmc.expression.standard.ExpressionReward;
@@ -10,6 +15,7 @@ import epmc.expression.standard.ExpressionTemporalGlobally;
 import epmc.expression.standard.ExpressionTemporalNext;
 import epmc.expression.standard.ExpressionTemporalRelease;
 import epmc.expression.standard.ExpressionTemporalUntil;
+import epmc.expression.standard.ExpressionType;
 import epmc.expression.standard.ExpressionTypeBoolean;
 import epmc.expression.standard.RewardType;
 import epmc.expression.standard.TimeBound;
@@ -93,18 +99,20 @@ public final class UtilPrismExpressionParser {
         }
     }
 
-    static ExpressionOperator newOperator(Operator operator, Expression... operands) {
-        return new ExpressionOperator.Builder()
-                .setOperator(operator)
-                .setOperands(operands)
-                .build();
-    }
-
     static ExpressionOperator newOperator(Operator operator, Expression operand1, Expression operand2, InfoExpression info) {
         Positional positional = info != null ? info.toPositional() : null;
         return new ExpressionOperator.Builder()
                 .setOperator(operator)
                 .setOperands(operand1, operand2)
+                .setPositional(positional)
+                .build();
+    }
+
+    static ExpressionOperator newOperator(Operator operator, Expression operand1, Expression operand2, Expression operand3, InfoExpression info) {
+        Positional positional = info != null ? info.toPositional() : null;
+        return new ExpressionOperator.Builder()
+                .setOperator(operator)
+                .setOperands(operand1, operand2, operand3)
                 .setPositional(positional)
                 .build();
     }
@@ -126,11 +134,8 @@ public final class UtilPrismExpressionParser {
         return newOperator(OperatorNot.NOT, expression, info);
     }
 
-    static Expression addInverse(Expression expression) {
-        return new ExpressionOperator.Builder()
-                .setOperator(OperatorAddInverse.ADD_INVERSE)
-                .setOperands(expression)
-                .build();
+    static Expression addInverse(Expression expression, InfoExpression info) {
+        return newOperator(OperatorAddInverse.ADD_INVERSE, expression, info);
     }
 
     static Expression or(Expression a, Expression b, InfoExpression info) {
@@ -170,58 +175,70 @@ public final class UtilPrismExpressionParser {
     }
 
     static ExpressionTemporalNext newTemporalNext(Expression operand,
-            TimeBound timeBound) {
+            TimeBound timeBound, InfoExpression info) {
         assert operand != null;
         assert timeBound != null;
+        Positional positional = info != null ? info.toPositional() : null;
         return new ExpressionTemporalNext.Builder()
                 .setOperand(operand)
                 .setTimeBound(timeBound)
+                .setPositional(positional)
                 .build();
     }
 
     static ExpressionTemporalFinally newTemporalFinally(Expression operand,
-            TimeBound timeBound) {
+            TimeBound timeBound, InfoExpression info) {
         assert operand != null;
         assert timeBound != null;
+        Positional positional = info != null ? info.toPositional() : null;
         return new ExpressionTemporalFinally.Builder()
                 .setOperand(operand)
                 .setTimeBound(timeBound)
+                .setPositional(positional)
                 .build();
     }
 
     static ExpressionTemporalGlobally newTemporalGlobally(Expression operand,
-            TimeBound timeBound) {
+            TimeBound timeBound, InfoExpression info) {
         assert operand != null;
         assert timeBound != null;
+        Positional positional = info != null ? info.toPositional() : null;
         return new ExpressionTemporalGlobally.Builder()
                 .setOperand(operand)
                 .setTimeBound(timeBound)
+                .setPositional(positional)
                 .build();
     }
 
     static ExpressionTemporalRelease newTemporalRelease(Expression operandLeft,
             Expression operandRight,
-            TimeBound timeBound) {
+            TimeBound timeBound,
+            InfoExpression info) {
         assert operandLeft != null;
         assert operandRight != null;
         assert timeBound != null;
+        Positional positional = info != null ? info.toPositional() : null;
         return new ExpressionTemporalRelease.Builder()
                 .setOperandLeft(operandLeft)
                 .setOperandRight(operandRight)
                 .setTimeBound(timeBound)
+                .setPositional(positional)
                 .build();
     }
 
     static ExpressionTemporalUntil newTemporalUntil(Expression operandLeft,
             Expression operandRight,
-            TimeBound timeBound) {
+            TimeBound timeBound,
+            InfoExpression info) {
         assert operandLeft != null;
         assert operandRight != null;
         assert timeBound != null;
+        Positional positional = info != null ? info.toPositional() : null;
         return new ExpressionTemporalUntil.Builder()
                 .setOperandLeft(operandLeft)
                 .setOperandRight(operandRight)
                 .setTimeBound(timeBound)
+                .setPositional(positional)
                 .build();
     }
 
@@ -287,6 +304,48 @@ public final class UtilPrismExpressionParser {
                 .build();
     }
 
+    static ExpressionIdentifier newIdentifier(Object identifier, InfoExpression info) {
+        Positional positional = info != null ? info.toPositional() : null;
+        return new ExpressionIdentifierStandard.Builder()
+                .setName(identifier.toString())
+                // TODO find out why setting the positional leads to problems in later code
+//                .setPositional(positional)
+                .build();
+    }
+
+    static ExpressionIdentifier newIdentifier(Object identifier) {
+        return new ExpressionIdentifierStandard.Builder()
+                .setName(identifier.toString())
+                .build();
+    }
+
+    static ExpressionLiteral newLiteral(Object identifier, ExpressionType type,
+            InfoExpression info) {
+        Positional positional = info != null ? info.toPositional() : null;
+        return new ExpressionLiteral.Builder()
+                .setValue(identifier.toString())
+                .setType(type)
+                .setPositional(positional)
+                .build();
+    }
+
+    static ExpressionLiteral newLiteral(Object identifier, ExpressionType type) {
+        return new ExpressionLiteral.Builder()
+                .setValue(identifier.toString())
+                .setType(type)
+                .build();
+    }
+
+    static ExpressionCoalition newCoalition(Expression quantifier, List<Expression> players,
+            InfoExpression info) {
+        Positional positional = info != null ? info.toPositional() : null;
+        return new ExpressionCoalition.Builder()
+                .setPlayers(players)
+                .setQuantifier(quantifier)
+                .setPositional(positional)
+                .build();
+    }
+    
     private UtilPrismExpressionParser() {
     }
 }
