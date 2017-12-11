d6ff57e7a95aaf175f75fb8616a468719e18db81
Ernst Moritz Hahn <ernst.moritz.hahn@gmx.de>
2017-12-11 15:06:25 +0000

    fix

--- plugins/prism-format/src/main/java/epmc/prism/model/convert/PRISM2JANIConverter.java
+++ plugins/prism-format/src/main/java/epmc/prism/model/convert/PRISM2JANIConverter.java
@@ -209,6 +209,7 @@ public final class PRISM2JANIConverter {
 
         convertExtensions();
 
+        globalVariablesArray = new Expression[0]; // avoid null pointer exception
         Variables globalVariables = buildGlobalVariables();
         globalVariablesArray = new Expression[globalVariables.size()];
         int varNr = 0;
