64c1e3b1fee30907937cfe879046f6910b9e98a4
Ernst Moritz Hahn <ernst.moritz.hahn@gmx.de>
2018-07-25 16:41:11 +0100

    hopefully works now...

--- plugins/param/src/main/c/gmp/util_gmp.c
+++ plugins/param/src/main/c/gmp/util_gmp.c
@@ -7,40 +7,47 @@
 
 static int DOUBLE_MANTISSA_BITS = 53;
 
-
+__attribute__ ((visibility("default")))
 int gmp_util_size_of_mpq() {
     return sizeof(mpq_t);
 }
 
+__attribute__ ((visibility("default")))
 int gmp_util_size_of_mpfr() {
     return sizeof(mpfr_t);
 }
 
+__attribute__ ((visibility("default")))
 void gmp_util_free_string(char *str) {
     assert(str != NULL);
     free(str);
 }
 
+__attribute__ ((visibility("default")))
 int gmp_util_mpq_get_int(mpq_t value) {
     assert(value != NULL);
     return mpz_get_si(mpq_numref(value));
 }
 
+__attribute__ ((visibility("default")))
 void gmp_util_mpq_set_int(mpq_t rop, int to) {
     assert(rop != NULL);
     mpq_set_si(rop, to, 1);
 }
 
+__attribute__ ((visibility("default")))
 char *gmp_util_mpq_get_num(mpq_t value) {
     assert(value != NULL);
     return mpz_get_str(NULL, 10, mpq_numref(value));
 }
 
+__attribute__ ((visibility("default")))
 char *gmp_util_mpq_get_den(mpq_t value) {
     assert(value != NULL);
     return mpz_get_str(NULL, 10, mpq_denref(value));
 }
 
+__attribute__ ((visibility("default")))
 int gmp_util_mpq_is_zero(mpq_t value) {
     assert(value != NULL);
     if (mpz_cmp_si(mpq_numref(value), 0) != 0) {
@@ -51,6 +58,7 @@ int gmp_util_mpq_is_zero(mpq_t value) {
     return 1;
 }
 
+__attribute__ ((visibility("default")))
 int gmp_util_mpq_is_one(mpq_t value) {
     assert(value != NULL);
     if (mpz_cmp_si(mpq_numref(value), 1) != 0) {
@@ -61,12 +69,14 @@ int gmp_util_mpq_is_one(mpq_t value) {
     return 1;
 }
 
+__attribute__ ((visibility("default")))
 int gmp_util_mpq_equals(mpq_t op1, mpq_t op2) {
     assert(op1 != NULL);
     assert(op2 != NULL);
     return mpq_equal(op1, op2);
 }
 
+__attribute__ ((visibility("default")))
 void gmp_util_init_array(mpq_t rop, int size) {
     assert(rop != NULL);
     assert(size >= 0);
@@ -75,6 +85,7 @@ void gmp_util_init_array(mpq_t rop, int size) {
     }
 }
 
+__attribute__ ((visibility("default")))
 void gmp_util_clear_array(mpq_t rop, int size) {
     assert(rop != NULL);
     assert(size >= 0);
@@ -83,6 +94,7 @@ void gmp_util_clear_array(mpq_t rop, int size) {
     }
 }
 
+__attribute__ ((visibility("default")))
 void gmp_util_mpq_array_set_int(mpq_t rop, int index, int to) {
     assert(rop != NULL);
     assert(index >= 0);
@@ -90,6 +102,7 @@ void gmp_util_mpq_array_set_int(mpq_t rop, int index, int to) {
     mpq_set_si(rop + index, to, 1);
 }
 
+__attribute__ ((visibility("default")))
 int gmp_util_mpq_array_equals(mpq_t op1, mpq_t op2, int length) {
     assert(op1 != NULL);
     assert(op2 != NULL);
@@ -102,6 +115,7 @@ int gmp_util_mpq_array_equals(mpq_t op1, mpq_t op2, int length) {
     return 1;
 }
 
+__attribute__ ((visibility("default")))
 void gmp_util_mpq_array_set(mpq_t rop, int index, mpq_t to) {
     assert(rop != NULL);
     assert(index >= 0);
@@ -109,6 +123,7 @@ void gmp_util_mpq_array_set(mpq_t rop, int index, mpq_t to) {
     mpq_set(rop + index, to);
 }
 
+__attribute__ ((visibility("default")))
 void gmp_util_mpq_array_get(mpq_t rop, int index, mpq_t to) {
     assert(rop != NULL);
     assert(index >= 0);
@@ -116,6 +131,7 @@ void gmp_util_mpq_array_get(mpq_t rop, int index, mpq_t to) {
     mpq_set(to, rop + index);
 }
 
+__attribute__ ((visibility("default")))
 void gmp_util_mpq_to_double_interval(double *result, mpq_t op, mpfr_t buffer) {
     assert(result != NULL);
     assert(op != NULL);
@@ -133,6 +149,7 @@ void gmp_util_mpq_to_double_interval(double *result, mpq_t op, mpfr_t buffer) {
     }
 }
 
+__attribute__ ((visibility("default")))
 void gmp_util_mpq_interval_to_double_interval(double *result, mpq_t op_left, mpq_t op_right, mpfr_t buffer) {
     assert(result != NULL);
     assert(op_left != NULL);
@@ -147,6 +164,7 @@ void gmp_util_mpq_interval_to_double_interval(double *result, mpq_t op_left, mpq
     *right = mpfr_get_d(buffer, MPFR_RNDU);
 }
 
+__attribute__ ((visibility("default")))
 void gmp_util_mpq_pow(mpq_t result, mpq_t op1, int op2) {
     assert(result != NULL);
     assert(op1 != NULL);
@@ -155,6 +173,7 @@ void gmp_util_mpq_pow(mpq_t result, mpq_t op1, int op2) {
     mpz_pow_ui(mpq_denref(result), mpq_denref(op1), op2);
 }
 
+__attribute__ ((visibility("default")))
 void gmp_util_mpq_max(mpq_t result, mpq_t op1, mpq_t op2) {
     assert(result != NULL);
     assert(op1 != NULL);
@@ -166,6 +185,7 @@ void gmp_util_mpq_max(mpq_t result, mpq_t op1, mpq_t op2) {
     }
 }
 
+__attribute__ ((visibility("default")))
 double gmp_util_mpq_get_double(mpq_t op, mpfr_t buffer) {
     assert(op != NULL);
     assert(buffer != NULL);
@@ -174,6 +194,7 @@ double gmp_util_mpq_get_double(mpq_t op, mpfr_t buffer) {
     return mpfr_get_d(buffer, MPFR_RNDN);
 }
 
+__attribute__ ((visibility("default")))
 void gmp_util_init_mpfr(mpfr_t x, int prec) {
     mpfr_init2(x, (mpfr_prec_t) prec);
 }
