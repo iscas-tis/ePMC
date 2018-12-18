178ef1e181c974d0ded7c6117b5495da879feaf1
Andrea Turrini <andrea.turrini@gmail.com>
2018-12-18 22:41:51 +0800

    Further processors

---
+++ plugins/jani-exporter/src/main/java/epmc/operator/JANIExporter_OperatorGtProcessor.java
@@ -0,0 +1,74 @@
+/****************************************************************************
+
+    ePMC - an extensible probabilistic model checker
+    Copyright (C) 2017
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ *****************************************************************************/
+
+package epmc.operator;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonValue;
+
+import epmc.expression.standard.ExpressionOperator;
+import epmc.jani.exporter.operatorprocessor.OperatorProcessor;
+import epmc.jani.exporter.processor.ProcessorRegistrar;
+import epmc.jani.model.UtilModelParser;
+
+/**
+ * @author Andrea Turrini
+ *
+ */
+public class JANIExporter_OperatorGtProcessor implements OperatorProcessor {
+    private final static String OP = "op";
+    private final static String LT = "<";
+    private final static String LEFT = "left";
+    private final static String RIGHT = "right";
+    
+
+    private ExpressionOperator expressionOperator = null;
+    
+    @Override
+    public OperatorProcessor setExpressionOperator(ExpressionOperator expressionOperator) {
+        assert expressionOperator != null;
+        
+        assert expressionOperator.getOperator().equals(OperatorGt.GT);
+    
+        this.expressionOperator = expressionOperator;
+        return this;
+    }
+
+    @Override
+    public JsonValue toJSON() {
+        assert expressionOperator != null;
+        
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+
+        // op1 > op2 ==> op2 < op1
+        builder.add(OP, LT);
+        
+        builder.add(LEFT, ProcessorRegistrar.getProcessor(expressionOperator.getOperand2())
+                .toJSON());
+        
+        builder.add(RIGHT, ProcessorRegistrar.getProcessor(expressionOperator.getOperand1())
+                .toJSON());
+        
+        UtilModelParser.addPositional(builder, expressionOperator.getPositional());
+        
+        return builder.build();
+    }
+}    
