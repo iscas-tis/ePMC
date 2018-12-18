69fe62a458205466bd354d23d0fdfa77703ca388
Andrea Turrini <andrea.turrini@gmail.com>
2018-12-18 13:48:25 +0800

    Further processors

---
+++ plugins/jani-exporter/src/main/java/epmc/jani/model/TimeProgressProcessor.java
@@ -0,0 +1,42 @@
+package epmc.jani.model;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonValue;
+
+import epmc.jani.exporter.processor.JANIProcessor;
+import epmc.jani.exporter.processor.ProcessorRegistrar;
+
+public class TimeProgressProcessor implements JANIProcessor {
+    private final static String EXP = "exp";
+    private final static String COMMENT = "comment";
+
+    private TimeProgress timeProgress = null;
+
+    @Override
+    public JANIProcessor setElement(Object component) {
+        assert component != null;
+        assert component instanceof TimeProgress; 
+
+        timeProgress = (TimeProgress) component;
+        return this;
+    }
+
+    @Override
+    public JsonValue toJSON() {
+        assert timeProgress != null;
+
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        
+        builder.add(EXP, ProcessorRegistrar.getProcessor(timeProgress.getExp())
+                .toJSON());
+//        ExpressionParser.generateExpression(model, timeProgress.getExp()));
+        
+        String comment = timeProgress.getComment();
+        if (comment != null) {
+            builder.add(COMMENT, comment);
+        }
+        
+        return builder.build();
+    }
+}
