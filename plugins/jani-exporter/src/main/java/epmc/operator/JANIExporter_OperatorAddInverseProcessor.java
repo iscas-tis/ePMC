cc2a7c521f74c8dbad7389d4911cbf528d78ab53
Andrea Turrini <andrea.turrini@gmail.com>
2018-12-18 23:11:27 +0800

    Further processors

---
+++ plugins/jani-exporter/src/main/java/epmc/operator/JANIExporter_OperatorAddInverseProcessor.java
@@ -0,0 +1,73 @@
+/****************************************************************************
+
+    ePMC - an extensible probabilistic model checker
+    Copyright (C) 2017
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ *****************************************************************************/
+
+package epmc.operator;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonValue;
+
+import epmc.expression.standard.ExpressionOperator;
+import epmc.jani.exporter.operatorprocessor.OperatorProcessor;
+import epmc.jani.exporter.processor.ProcessorRegistrar;
+import epmc.jani.model.UtilModelParser;
+
+/**
+ * @author Andrea Turrini
+ *
+ */
+public class JANIExporter_OperatorAddInverseProcessor implements OperatorProcessor {
+    private final static String OP = "op";
+    private final static String ADD_INVERSE = "-";
+    private final static String LEFT = "left";
+    private final static String RIGHT = "right";
+    
+
+    private ExpressionOperator expressionOperator = null;
+    
+    @Override
+    public OperatorProcessor setExpressionOperator(ExpressionOperator expressionOperator) {
+        assert expressionOperator != null;
+        
+        assert expressionOperator.getOperator().equals(OperatorAddInverse.ADD_INVERSE);
+    
+        this.expressionOperator = expressionOperator;
+        return this;
+    }
+
+    @Override
+    public JsonValue toJSON() {
+        assert expressionOperator != null;
+        
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+
+        // add_inverse(x) ==> -x
+        builder.add(OP, ADD_INVERSE);
+        
+        builder.add(LEFT, 1);
+        
+        builder.add(RIGHT, ProcessorRegistrar.getProcessor(expressionOperator.getOperand1())
+                .toJSON());
+        
+        UtilModelParser.addPositional(builder, expressionOperator.getPositional());
+        
+        return builder.build();
+    }
+}    
---
+++ plugins/jani-exporter/src/main/java/epmc/operator/JANIExporter_OperatorFloorProcessor.java
@@ -0,0 +1,69 @@
+/****************************************************************************
+
+    ePMC - an extensible probabilistic model checker
+    Copyright (C) 2017
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ *****************************************************************************/
+
+package epmc.operator;
+
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonValue;
+
+import epmc.expression.standard.ExpressionOperator;
+import epmc.jani.exporter.operatorprocessor.OperatorProcessor;
+import epmc.jani.exporter.processor.ProcessorRegistrar;
+import epmc.jani.model.UtilModelParser;
+
+/**
+ * @author Andrea Turrini
+ *
+ */
+public class JANIExporter_OperatorFloorProcessor implements OperatorProcessor {
+    private final static String OP = "op";
+    private final static String FLOOR = "floor";
+    private final static String EXP = "exp";
+    
+
+    private ExpressionOperator expressionOperator = null;
+    
+    @Override
+    public OperatorProcessor setExpressionOperator(ExpressionOperator expressionOperator) {
+        assert expressionOperator != null;
+        
+        assert expressionOperator.getOperator().equals(OperatorFloor.FLOOR);
+    
+        this.expressionOperator = expressionOperator;
+        return this;
+    }
+
+    @Override
+    public JsonValue toJSON() {
+        assert expressionOperator != null;
+        
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+
+        builder.add(OP, FLOOR);
+        
+        builder.add(EXP, ProcessorRegistrar.getProcessor(expressionOperator.getOperand1())
+                .toJSON());
+        
+        UtilModelParser.addPositional(builder, expressionOperator.getPositional());
+        
+        return builder.build();
+    }
+}    
