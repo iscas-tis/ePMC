9223a396a46e64a7440a350f16e51f9fc48d7ae0
Andrea Turrini <andrea.turrini@gmail.com>
2019-04-12 11:25:54 +0800

    Fixed plugin order

--- distributions/qmc/build.sh
+++ distributions/qmc/build.sh
@@ -9,11 +9,11 @@ prepare_plugin expression-basic
 prepare_plugin graph
 prepare_plugin algorithm
 prepare_plugin graphsolver
+prepare_plugin jani-interaction
+prepare_plugin command-help
 prepare_plugin command-check
 prepare_plugin command-explore
-prepare_plugin command-help
 prepare_plugin jani-model
-prepare_plugin jani-interaction
 prepare_plugin prism-format
 prepare_plugin qmc
 prepare_plugin propertysolver-propositional
